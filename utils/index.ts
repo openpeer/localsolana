9fe9ef941a09665784113a28d4b7176f5b4303e3
Garjpreet Singh <megamindinnovations@gmail.com>
2024-09-14 11:48:58 +0530

    Merged code

--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ import { List, Token } from 'models/types';
 import snakecaseKeys from 'snakecase-keys';
 
 
-export const smallWalletAddress = (address: `0x${string}`, length = 4): string =>
+export const smallWalletAddress = (address: string, length = 4): string =>
 	`${address.substring(0, length)}..${address.substring(address.length - length)}`;
 
 export const truncate = (num: number, places: number) => Math.trunc(num * 10 ** places) / 10 ** places;
