a05343d740146ec1c2eb9ed7661d022f559cb7d1
George Donnelly <me@georgedonnelly.com>
2024-12-17 09:24:33 -0500

    use old connection

--- hooks/transactions/deposit/useDepositFunds.ts
+++ hooks/transactions/deposit/useDepositFunds.ts
@@ -4,11 +4,10 @@ import { UseDepositFundsProps } from '../types';
 import useDepositWithGas from './useDepositWithGas';
 import useGaslessDepositFunds from './useGaslessDepositFunds';
 import { useDynamicContext } from '@dynamic-labs/sdk-react-core';
-import { getSolanaConnection } from '@/utils/solana';
 
 const useDepositFunds = ({ amount, token, contract }: UseDepositFundsProps) => {
     const nativeToken = token.address === PublicKey.default.toBase58();
-    const connection = getSolanaConnection();
+    const connection = new Connection(clusterApiUrl('mainnet-beta')); // Adjust the cluster as needed
     const { primaryWallet } = useDynamicContext();
 
     const withGasCall = useDepositWithGas({
